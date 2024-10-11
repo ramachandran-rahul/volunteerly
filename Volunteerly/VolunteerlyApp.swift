fd7b39fa0fb7b009f6180e0c6342f568952490a9
ramachandran-rahul <ramachandran.k.rahul@gmail.com>
2024-10-11 13:45:09 +1100

    Initial Commit

---
+++ Volunteerly/VolunteerlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteerlyApp.swift
+//  Volunteerly
+//
+//  Created by Rahul Ramachandran on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VolunteerlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
